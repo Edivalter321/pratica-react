10db728a017f3a0d9efc386695c18ada62066ded
edivalter <junioredivalter25@gmail.com>
2024-04-10 15:04:02 -0300

    estilizações

--- src/Conteudo/Conteudo.jsx
+++ src/Conteudo/Conteudo.jsx
@@ -3,6 +3,9 @@ import imagemTenis from "./Assets/tennis_g.png";
 import imagemOrnament from "./Assets/Ornament 11.png";
 import imagemFogo from "./Assets/fogo.png";
 import imagemPontos from "./Assets/pontos.png";
+import imagemCamisa from "./Assets/Mask group.png";
+import imagemTennis from "./Assets/tennis_02.png";
+import imagemHeaderPhone from "./Assets/headerphone.png";
 
 function Conteudo() {
   return (
@@ -31,12 +34,31 @@ function Conteudo() {
         </div>
       </div>
       <div className="caixa_02">
-        <p className="">Coleção em Destaque</p>
+        <p className="colecao">Coleção em destaque</p>
         <div className="caixas">
-          <div className="caixas_01"></div>
-          <div className="caixas_02"></div>
-          <div className="caixas_02"></div>
+          <div className="caixas_a">
+            <p className="yelow">30% OFF</p>
+            <h3 className="preco">Novo Drop Supreme</h3>
+            <button className="botao_compra">Comprar</button>
+            <img src={imagemCamisa} alt="camisa" className="imagem" />
+          </div>
+          <div className="caixas_a">
+            <p className="yelow">30% OFF</p>
+            <h3 className="preco">Coleção Adidas</h3>
+            <button className="botao_compra">Comprar</button>
+            <img src={imagemTennis} alt="tenis" className="imagem" />
+          </div>
+
+          <div className="caixas_a">
+            <p className="yelow">30% OFF</p>
+            <h3 className="preco">Novo Beats Bass</h3>
+            <button className="botao_compra">Comprar</button>
+            <img src={imagemHeaderPhone} alt="headPhone" className="imagem" />
+          </div>
         </div>
+      </div>
+      <div className="caixa_03">
+        <p className="colecao_02">Coleção em destaque</p>
       </div>
     </>
   );
